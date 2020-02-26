79fe131a0e83df4fed1fc7e1870fabfd17825b3f
telefunk <telewebb@gmail.com>
2020-02-26 17:24:31 -0500

    Finished implementing aesthetic functions

--- RACharacterGenerator/Character.cs
+++ RACharacterGenerator/Character.cs
@@ -7,23 +7,63 @@ namespace RACharacterGenerator
     public class Character
     {
         static Random dice = new Random();
-        public int Age = GenAge();
-        public string Sex = GenSex();
-        public string Complexion = GenComplexion();
+        public int Age;
+        public string Sex;
+        public string Handedness;
+        public string Complexion;
+        public string BodyType;
+        public string DistinctiveTrait;
 
-        static int GenAge()
+        public void GenChar()
+        {
+            Age = GenAge();
+            Sex = GenSex();
+            Handedness = GenHandedness();
+            Complexion = GenComplexion();
+            BodyType = GenBodyType();
+            DistinctiveTrait = GenDistinctTrait();
+        }
+        int GenAge()
         {
             return 20 + dice.Next(1, 11);
         }
-        static string GenSex()
+        string GenSex()
         {
             if (dice.Next(1, 101) < 51) return "Male";
             else return "Female";
         }
-        static string GenComplexion()
+        string GenHandedness()
+        {
+            int roll = dice.Next(1, 101);
+            if (roll <= 89) return "Right Handed";
+            else if (roll <= 99) return "Left Handed";
+            else return "Ambidextrous";
+        }
+        string GenComplexion()
         {
             string[] complexion = new string[10] { "Pale", "Fair", "Golden", "Rosy", "Ruddy", "Tan", "Olive", "Dark", "Swarthy", "Dusky" };
             return complexion[(dice.Next(100)/10)];
+        }
+        string GenBodyType()
+        {
+            string[,] bodyType = new string[7, 2] { { "Scrawny", "Petite" }, { "Lean", "Slender" }, { "Athletic", "Fit" }, { "Average", "Average" }, { "Brawny", "Muscular" }, { "Stocky", "Buxom" }, { "Heavy", "Plump" } };
+            int roll = dice.Next(1, 101);
+            int sexIndex;
+            if (Sex == "Male") sexIndex = 0;
+            else sexIndex = 1;
+            if (roll <= 10) return bodyType[0, sexIndex];
+            else if (roll <= 25) return bodyType[1, sexIndex];
+            else if (roll <= 40) return bodyType[2, sexIndex];
+            else if (roll <= 60) return bodyType[3, sexIndex];
+            else if (roll <= 75) return bodyType[4, sexIndex];
+            else if (roll <= 90) return bodyType[5, sexIndex];
+            else return bodyType[6, sexIndex];
+        }
+        string GenDistinctTrait()
+        {
+            string[] trait = new string[49] { "Androgynous.", "Animal tattoos.", "Bald head.", "Bow-Legged.", "Close-set eyes.", "Crooked nose.", "Crooked teeth.", "Excess body hair.", "Exotropic eyes.", "Extensive liquid burn marks.", "Extreme scarring on legs.", "Eyebrow piercings.", "Gang tattoos.", "Glasgow Smile.", "Healed chemical burn marks.", "Heavy scarring on face.", "Huge scarring on back.", "Lip piercing.", "Long fingers.", "Major electrical burn marks.", "Military tattoos.", "Missing ear.", "Missing finger.", "Missing teeth.", "Missing toe.", "Mythological tattoos.", "Nervous facial tic.", "Nose piercing.", "Old brand on face.", "Old brand on neck.", "Old brands on arms.", "Old brands on arms.", "Old brands on torso.", "Overbite.", "Plant tattoos.", "Pockmarked skin.", "Protruding brow and chin.", "Religious tattoos.", "Scarring on arms.", "Severe flame burn marks.", "Stain-mark on arm.", "Stain-mark on face.", "Stain-mark on neck.", "Stain-mark on torso.", "Terrible scarring on chest.", "Tribal tattoos.", "Underbite.", "Unpleasant odour.", "Vitiligo." };
+            int roll = dice.Next(100);
+            return trait[roll / 2];
         }
     }
 }
--- RACharacterGenerator/Program.cs
+++ RACharacterGenerator/Program.cs
@@ -6,14 +6,23 @@ namespace RACharacterGenerator
     {
         static void Main(string[] args)
         {
-            Character character = new Character();
+            Character character1 = new Character();
+            character1.GenChar();
+            Character character2 = new Character();
+            character2.GenChar();
 
-            Console.WriteLine($"Age is: {character.Age}");
-            Console.WriteLine($"Age is: {character.Age}");
-            Console.WriteLine($"Sex is: {character.Sex}");
-            Console.WriteLine($"Sex is: {character.Sex}");
-            Console.WriteLine($"Complexion is: {character.Complexion}");
-            Console.WriteLine($"Complexion is: {character.Complexion}");
+            //Console.WriteLine($"Character 1 age is: {character1.Age}");
+            //Console.WriteLine($"Character 2 age is: {character2.Age}");
+            //Console.WriteLine($"Character 1 sex is: {character1.Sex}");
+            //Console.WriteLine($"Character 2 sex is: {character2.Sex}");
+            //Console.WriteLine($"Character 1 handedness is: {character1.Handedness}");
+            //Console.WriteLine($"Character 2 handedness is: {character2.Handedness}");
+            //Console.WriteLine($"Character 1 complexion is: {character1.Complexion}");
+            //Console.WriteLine($"Character 2 complexion is: {character2.Complexion}");
+            //Console.WriteLine($"Character 1 body type is: {character1.BodyType}");
+            //Console.WriteLine($"Character 2 body type is: {character2.BodyType}");
+            //Console.WriteLine($"Character 1 distinctive trait is: {character1.DistinctiveTrait}");
+            //Console.WriteLine($"Character 2 distinctive trait is: {character2.DistinctiveTrait}");
         }
     }
 }
