ba9013d490362093e28107ec3cd5dcab5a07d899
telefunk <telewebb@gmail.com>
2020-02-26 19:23:00 -0500

    First two skill questions built

--- RACharacterGenerator/Character.cs
+++ RACharacterGenerator/Character.cs
@@ -7,6 +7,9 @@ namespace RACharacterGenerator
     public class Character
     {
         static Random dice = new Random();
+        public int[] SkillLevel = new int[20] { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 };
+        public string[] SkillName = new string[20] { "Athletics", "Burglary", "Constitution", "Drive", "Fight", "Perception", "Might", "Shoot", "Stealth", "Broad-Craft", "Deceive", "Diplomacy", "Fine-Craft", "Intimidate", "Intuition", "Investigate", "Logic", "Special", "Wealth", "Will" };
+        public string[,] HistoryQs = new string[27, 2];
         public int Age;
         public string Sex;
         public string Handedness;
@@ -22,6 +25,8 @@ namespace RACharacterGenerator
             Complexion = GenComplexion();
             BodyType = GenBodyType();
             DistinctiveTrait = GenDistinctTrait();
+            HistQ0();
+            HistQ1();
         }
         int GenAge()
         {
@@ -64,6 +69,108 @@ namespace RACharacterGenerator
             string[] trait = new string[49] { "Androgynous.", "Animal tattoos.", "Bald head.", "Bow-Legged.", "Close-set eyes.", "Crooked nose.", "Crooked teeth.", "Excess body hair.", "Exotropic eyes.", "Extensive liquid burn marks.", "Extreme scarring on legs.", "Eyebrow piercings.", "Gang tattoos.", "Glasgow Smile.", "Healed chemical burn marks.", "Heavy scarring on face.", "Huge scarring on back.", "Lip piercing.", "Long fingers.", "Major electrical burn marks.", "Military tattoos.", "Missing ear.", "Missing finger.", "Missing teeth.", "Missing toe.", "Mythological tattoos.", "Nervous facial tic.", "Nose piercing.", "Old brand on face.", "Old brand on neck.", "Old brands on arms.", "Old brands on arms.", "Old brands on torso.", "Overbite.", "Plant tattoos.", "Pockmarked skin.", "Protruding brow and chin.", "Religious tattoos.", "Scarring on arms.", "Severe flame burn marks.", "Stain-mark on arm.", "Stain-mark on face.", "Stain-mark on neck.", "Stain-mark on torso.", "Terrible scarring on chest.", "Tribal tattoos.", "Underbite.", "Unpleasant odour.", "Vitiligo." };
             int roll = dice.Next(100);
             return trait[roll / 2];
+        }
+        void IncreaseSkills(int a, int b)
+        {
+            SkillLevel[a] += dice.Next(1, 6);
+            SkillLevel[b] += dice.Next(1, 6);
+            return;
+        }
+        void HistQ0()
+        {
+            HistoryQs[0, 0] = "What was your earliest memory?";
+            switch (dice.Next(100) / 10)
+            {
+                case 0:
+                    HistoryQs[0, 1] = "A loud explosion and then silence.";
+                    IncreaseSkills(3, 13);
+                    break;
+                case 1:
+                    HistoryQs[0, 1] = "Running for my life.";
+                    IncreaseSkills(0, 5);
+                    break;
+                case 2:
+                    HistoryQs[0, 1] = "Getting caught stealing.";
+                    IncreaseSkills(1, 19);
+                    break;
+                case 3:
+                    HistoryQs[0, 1] = "Eating like there's no tomorrow.";
+                    IncreaseSkills(2, 18);
+                    break;
+                case 4:
+                    HistoryQs[0, 1] = "Crying because I got hopelessly lost.";
+                    IncreaseSkills(12, 15);
+                    break;
+                case 5:
+                    HistoryQs[0, 1] = "Being covered in someone else's blood.";
+                    IncreaseSkills(9, 7);
+                    break;
+                case 6:
+                    HistoryQs[0, 1] = "My parents telling me it will all be OK.";
+                    IncreaseSkills(10, 6);
+                    break;
+                case 7:
+                    HistoryQs[0, 1] = "The joyful laughter of my family.";
+                    IncreaseSkills(11, 17);
+                    break;
+                case 8:
+                    HistoryQs[0, 1] = "A vicious battle. I don't know who won.";
+                    IncreaseSkills(4, 16);
+                    break;
+                case 9:
+                    HistoryQs[0, 1] = "Playing hide and seek with my friends.";
+                    IncreaseSkills(14, 8);
+                    break;
+            }
+            return;
+        }
+        void HistQ1()
+        {
+            HistoryQs[1, 0] = "Where did you grow up?";
+            switch (dice.Next(100) / 10)
+            {
+                case 0:
+                    HistoryQs[1, 1] = "On the streets.";
+                    IncreaseSkills(8, 4);
+                    break;
+                case 1:
+                    HistoryQs[1, 1] = "On the open road, always traveling.";
+                    IncreaseSkills(3, 11);
+                    break;
+                case 2:
+                    HistoryQs[1, 1] = "On a farm, in the muck.";
+                    IncreaseSkills(9, 0);
+                    break;
+                case 3:
+                    HistoryQs[1, 1] = "Near a church, helping the priests.";
+                    IncreaseSkills(15, 6);
+                    break;
+                case 4:
+                    HistoryQs[1, 1] = "In a mansion, waited upon hand and foot.";
+                    IncreaseSkills(13, 1);
+                    break;
+                case 5:
+                    HistoryQs[1, 1] = "Near a library that I always visited.";
+                    IncreaseSkills(16, 12);
+                    break;
+                case 6:
+                    HistoryQs[1, 1] = "Wherever I could.";
+                    IncreaseSkills(7, 5);
+                    break;
+                case 7:
+                    HistoryQs[1, 1] = "In a busy shop, constantly underfoot.";
+                    IncreaseSkills(19, 18);
+                    break;
+                case 8:
+                    HistoryQs[1, 1] = "In a comfortable urban home.";
+                    IncreaseSkills(17, 14);
+                    break;
+                case 9:
+                    HistoryQs[1, 1] = "On the stage, performing every night.";
+                    IncreaseSkills(10, 2);
+                    break;
+            }
+            return;
         }
     }
 }
--- RACharacterGenerator/Program.cs
+++ RACharacterGenerator/Program.cs
@@ -11,6 +11,20 @@ namespace RACharacterGenerator
             Character character2 = new Character();
             character2.GenChar();
 
+            Console.WriteLine($"Char 1 Q1: {character1.HistoryQs[0, 0]} Answer: {character1.HistoryQs[0, 1]}");
+            Console.WriteLine($"Char 1 Q2: {character1.HistoryQs[1, 0]} Answer: {character1.HistoryQs[1, 1]}");
+            for(int i = 0; i < character1.SkillLevel.Length; i++)
+            {
+                Console.WriteLine($"{character1.SkillName[i]}: {character1.SkillLevel[i]}");
+            }
+
+            Console.WriteLine($"Char 2 Q1: {character2.HistoryQs[0, 0]} Answer: {character2.HistoryQs[0, 1]}");
+            Console.WriteLine($"Char 2 Q2: {character2.HistoryQs[1, 0]} Answer: {character2.HistoryQs[1, 1]}");
+            for (int i = 0; i < character2.SkillLevel.Length; i++)
+            {
+                Console.WriteLine($"{character2.SkillName[i]}: {character2.SkillLevel[i]}");
+            }
+
             //Console.WriteLine($"Character 1 age is: {character1.Age}");
             //Console.WriteLine($"Character 2 age is: {character2.Age}");
             //Console.WriteLine($"Character 1 sex is: {character1.Sex}");
